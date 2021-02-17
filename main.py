0e6dfaccd0c33711c8e78fb18700ba2a0bdcb9ac
Giulio De Matteis <giuliod35@gmail.com>
2021-02-17 16:24:58 +0100

    Initialize projects, add and remove packages

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import sys
-from Project.Manager import initProject
+from Project.Manager import projectInit, projectCompile, runScript
+from Package.Manager import packageAdd, packageRemove
 
 def displayVersion():
     print('Formula v0.0.1')
@@ -8,14 +9,38 @@ def displayVersion():
 parser = argparse.ArgumentParser(description='Fortran Package Manager.')
 
 parser.add_argument('--init', '-i',
-                    dest='initProject',
+                    dest='projectName',
                     help='Start a Fortran project in the current folder')
+
+parser.add_argument('--add', '-a',
+                    dest='packageName',
+                    help='Add a module')
+
+parser.add_argument('--remove', '-R',
+                    dest='packageNameToRemove',
+                    help='Remove a module')
 
 parser.add_argument('--version', '-v',
                     dest='displayVersion',
                     action='store_const',
                     const=True,
                     help='Display Formula version')
+        
+parser.add_argument('--run', '-r',
+                    dest='scriptName',
+                    help='Run scripts located in config.yaml')
+
+parser.add_argument('--start',
+                    dest='runStart',
+                    action='store_const',
+                    const=True,
+                    help='An alias for --run start')
+
+parser.add_argument('--compile', '-c',
+                    dest='projectCompile',
+                    action='store_const',
+                    const=True,
+                    help='Compile Fortran project')
 
 args = parser.parse_args()
 nargs = len(sys.argv)
@@ -26,6 +51,16 @@ if nargs == 1:
 elif nargs == 2:
     if args.displayVersion:
         displayVersion()
+    elif args.projectCompile:
+        projectCompile()
+    elif args.runStart:
+        runScript('start')
 elif nargs == 3:
-    if args.initProject:
-        initProject(args.initProject)
+    if args.projectName:
+        projectInit(args.projectName)
+    elif args.scriptName:
+        runScript(args.scriptName)
+    elif args.packageName:
+        packageAdd(args.packageName)
+    elif args.packageNameToRemove:
+        packageRemove(args.packageNameToRemove)
